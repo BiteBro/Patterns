ca88c85df0ed3f824118b9afa6316719f921ad39
Rozhnevskiy Alexey <ralewww@gmail.com>
2023-08-06 23:18:55 +0300

    ФAdded some patterns

--- Adapter/src/main/java/org/example/Main.java
+++ Adapter/src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Repository r = new Adapter();
+        r.insert();
+        r.delete();
 
+        System.out.println();
+
+        Repository r2 = new AdapterWithoutExtends(new ServiceImpl());
+        r2.insert();
+        r2.delete();
     }
 }
--- Observer/src/main/java/org/example/publisher/Boss.java
+++ Observer/src/main/java/org/example/publisher/Boss.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Boss {
-    private List<CallMes> subscrs = new ArrayList();
+    private final List<CallMes> subscrs = new ArrayList<>();
     private String message = "New message";
 
     public void addSubscr(CallMes subscr){
